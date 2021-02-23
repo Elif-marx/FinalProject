b559c2b312d15f7a62842f7d59b6a90ee528028c
Elif-marx <elifnazkaya_01@hotmail.com>
2021-02-23 14:24:18 +0300

    17 şubat

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -18,13 +22,10 @@ namespace Business.Concrete
         {
             _ProductDal = productDal;
         }
-
-        public IResult Add(Product product)
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Add(Product product)//business codes validation
         {
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            // ValidationTool.Validate(new ProductValidator(), product);
             _ProductDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
